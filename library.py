afffc9d9fd1956bccc36747044426605e39ffa02
Vanessa <goodfairy24@gmail.com>
2024-03-04 22:15:17 +0000

    Added comments to explain my code

--- library.py
+++ library.py
@@ -5,13 +5,16 @@ Start
 3. Write functions for each menu option
 End
 '''
-
+#a dictionary of user accounts showing number of books borrowed
+#and the total amount of fines
 user_account_dict = {
     'Sarah':{'borrowed books':[],'fines':0},
      'Tony':{'borrowed books':[],'fines':0},
     'Ellie':{'borrowed books':[],'fines':0},
 }
 
+#a dictionay of books available from the library
+#who the book is currently being borrowed by and how many copies available
 books_dict = {
     'Harry Potter':{'borrowed by':[],'copies available':5},
     'Horrid Henry':{'borrowed by':[],'copies available':6},
@@ -20,10 +23,17 @@ books_dict = {
     'Beauty and the Beast':{'borrowed by':[],'copies available':3},
 }
 
+#a function to add a new user to the library
 def new_user(name):
     user_account_dict[name] = {'borrowed books':[],'fines':0}
     print(f'Welcome {user_name}. You can now borrow a book.')
 
+'''a function to borrow a book
+the function will search through the books dictionary to see if there is a copy available
+it will also update the user dictionary and the book dictionary to say who has the book
+it will not let the user borrow the book if they already have the maximum number of 
+books on loan
+it will not accept invalid book titles or usernames'''
 def borrow_book(book_title, user):
     if books_dict[book_title]['copies available'] > 0 and len(user_account_dict[user]['borrowed books']) < 5:
         books_dict[book_title]['copies available']-= 1
@@ -38,7 +48,12 @@ def borrow_book(book_title, user):
     else:
         print('Please enter a valid book title or username')
     
-
+'''a function to return a book
+it will update the book dictionary to remove the lender and add a copy
+it will update the user dictionary by removing the book and adding 1 to the
+number of books they are able to borrow
+a fine is added if the book is not returned on time and a menu link 
+prints to remind the user that they can check their outstanding fines'''
 def return_book(book_title, user, fine):
     books_dict[book_title]['copies available']+= 1
     books_dict[book_title]['borrowed by'].remove(user)
@@ -52,10 +67,11 @@ def return_book(book_title, user, fine):
     else:
         print('Please enter a valid book title or username')
 
-
+#a function to check the amount of outstanding fines
 def check_fines(user):
     print(f'Your fine amount is £{user_account_dict[user]}')
     
+#user menu on in a while loop so they can choose multiple functions or exit
 while True: 
     menu = '''Welcome to the library. What would you like to do today?
          1. Set up a new account
